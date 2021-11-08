b90d23a66d8f83b7ca288632861d24fb33699921
GalacticKittenSS <daniel05james@gmail.com>
2021-11-08 10:58:50 +0000

    Added Buffer Layouts

--- Nebula/src/Nebula/Application.cpp
+++ Nebula/src/Nebula/Application.cpp
@@ -7,6 +7,25 @@
 namespace Nebula {
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderTypetoOpenGL(ShaderDataType type) {
+		switch (type) {
+			case ShaderDataType::Float:		return GL_FLOAT;
+			case ShaderDataType::Float2:	return GL_FLOAT;
+			case ShaderDataType::Float3:	return GL_FLOAT;
+			case ShaderDataType::Float4:	return GL_FLOAT;
+			case ShaderDataType::Int:		return GL_INT;
+			case ShaderDataType::Int2:		return GL_INT;
+			case ShaderDataType::Int3:		return GL_INT;
+			case ShaderDataType::Int4:		return GL_INT;
+			case ShaderDataType::Mat3:		return GL_FLOAT;
+			case ShaderDataType::Mat4:		return GL_FLOAT;
+			case ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		NB_ASSERT(false, "Unknow Shader Data Type!");
+		return 0;
+	}
+
 	Application::Application() {
 		NB_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -20,21 +39,39 @@ namespace Nebula {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[2 * 3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f,  0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.5f,  0.5f, 0.0f,
-			-0.5f,  0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f,	0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f,	0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-		
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "position"},
+				{ShaderDataType::Float4, "colour"}
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			
+			glVertexAttribPointer(
+				index, 
+				element.GetComponentCount(), 
+				ShaderTypetoOpenGL(element.Type), 
+				element.Normalized ? GL_TRUE: GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset
+			);
+
+			index++;
+		}
 		
 		uint32_t indices[3] = { 0, 1, 2 };
 
@@ -42,27 +79,31 @@ namespace Nebula {
 
 		std::string vertexSrc = R"(
 			#version 330 core
-			
+
 			layout(location = 0) in vec3 position;
-			
+			layout(location = 1) in vec4 colour;
+
 			out vec3 outPos;
+			out vec4 outCol;
 			
 			void main() {
 				outPos = position;
-				gl_Position = vec4(position, 1.0);	
+				outCol = colour;
+				gl_Position = vec4(position, 1.0);
 			}
 		)";
 
 		std::string fragmentSrc = R"(
 			#version 330 core
-			
+
 			layout(location = 0) out vec4 colour;
 
 			in vec3 outPos;
+			in vec4 outCol;
 
-			void main()
-			{
+			void main() {
 				colour = vec4(outPos * 0.5 + 0.5, 1.0);
+				colour = outCol;
 			}
 		)";
 
